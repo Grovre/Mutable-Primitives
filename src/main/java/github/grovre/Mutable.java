0d035e4e7ff866c0433e01eeb6077e98dcd14713
Grovre <50428844+Grovre@users.noreply.github.com>
2022-05-03 19:10:45 -0500

    test update and Mutable

--- src/main/java/github/grovre/Mutable.java
+++ src/main/java/github/grovre/Mutable.java
@@ -1,11 +1,21 @@
 package github.grovre;
 
+import github.grovre.mutables.*;
 import lombok.Data;
+import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 @Data
 public abstract class Mutable {
+
+    @Getter
+    private static final List<Class<? extends Mutable>> mutableClasses =
+                    Arrays.asList(MutableBoolean.class, MutableByte.class,
+                    MutableChar.class, MutableDouble.class, MutableFloat.class,
+                    MutableInteger.class, MutableLong.class, MutableShort.class);
 
     protected final static Random random = new Random();
 
---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import github.grovre.interfaces.AtomicCapable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        List<AtomicCapable> mutables = new ArrayList<>();
+
+    }
+}
+
